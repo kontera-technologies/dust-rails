86e3a18acd3f0e43a64a87ca7d495e6c29fbafae
Ido Abramovich <github.sf17@abramovich.info>
2013-12-09 17:42:08 +0200

    make sure engine loads after sporkets and does nothing if no assets

--- lib/dust-rails/rails/engine.rb
+++ lib/dust-rails/rails/engine.rb
@@ -3,8 +3,10 @@ require 'rails/engine'
 module Dust
   module Rails
     class Engine < ::Rails::Engine
-      initializer :register_dustjs, :group => :all do |app|
-        app.assets.register_engine '.dust', DustTemplate
+      initializer 'dustjs.register', :after => "sporckets.environment", :group => :all do |app|
+        next unless app.assets
+
+        app.assets.register_engine('.dust', DustTemplate)
       end
     end
   end
